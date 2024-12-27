226f1e8516f1916b3ec2569066c7fcdc8f78bde4
95560384 <luisguille2289@gmail.com>
2024-12-26 22:49:38 -0500

    mejoras y fixed

--- Curso frontEnd/tshirt/tshirt.js
+++ Curso frontEnd/tshirt/tshirt.js
@@ -3,13 +3,12 @@ const productsList = document.querySelector('.tshirts');
 
 // 2. El componente carrito debera sumar los elementos que agregaste al carrito y mostrar el total
 const cart = JSON.parse(localStorage.getItem('cart')) || []
-console.log("CARRO",cart)
 
 const valorTotal = document.querySelector('#total')
 
 
 /**
- * Funcion para agregar elementos al Carrito broh
+ * Funcion para agregar elementos al Carrito
  */
 productsList.addEventListener('click', event => {
 	if (event.target.classList.contains('btn-add-cart')) {
@@ -25,15 +24,32 @@ productsList.addEventListener('click', event => {
         console.log("AGREGADO",cart)
         cart.push(shirt)
         localStorage.setItem('cart',JSON.stringify(cart));
+        window.location.reload();
     }
+    actualizarNumeroCarrito();
 })
 
-// Disparar un Evento cuando haces click en el carrito, y cuando se invoca el evento, disparar un ALERT en pantalla
+
 btnCart.addEventListener('click', event =>{
     // evento disparado
-
     // llamar a la funcion calcularTotal y pasarle como argumento el carrito que llenamos con el evento addcart
     //const valorTotal = calcularTotal(cart)
-    //alert(valorTotal)
    window.location.href="http://127.0.0.1:5500/tshirt/BuyCart.index.html";
 })
+
+const cuentaCarritoElement = document.getElementById('count-cart').innerHTML = `(${cart.length})`;
+function actualizarNumeroCarrito() {
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+    const count = cart.reduce((acum, current) => {
+        const shirtCount = current?.shirt; 
+        return acum + shirtCount;
+    }, 0);
+    //const count = cart.reduce((acum, current) => acum+current.shirt,0 )
+    cuentaCarritoElement.innerText = count;
+    
+  }
+  
+  actualizarNumeroCarrito();
+
+  
+
